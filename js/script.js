4d52dce916a3d628e85ca0d22e6eec690bf09a4f
unknown <yadavanjaliyadav1124@gmail.com>
2026-09-22 13:33:45 +0530

    made fix things

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 console.log("Portfolio Loaded Successfully");
 
+// Project buttons
 const buttons = document.querySelectorAll(".btn-outline-primary");
 
-buttons.forEach(btn=>{
-btn.addEventListener("click",()=>{
-alert("Project details will be added soon!");
-});
-});
-console.log("Portfolio Loaded Successfully");
-
-const buttons = document.querySelectorAll(".btn-outline-primary");
-
-buttons.forEach(btn=>{
-    btn.addEventListener("click",()=>{
-        alert("Project details will be added soon!");
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // Do nothing here.
+        // Bootstrap will open the correct project modal.
     });
 });
+
 
 // Typing Animation
 
@@ -30,38 +24,47 @@ let charIndex = 0;
 
 function typeEffect() {
 
-    if(charIndex < text[index].length){
+    if (charIndex < text[index].length) {
 
         document.getElementById("typing").innerHTML +=
-        text[index].charAt(charIndex);
+            text[index].charAt(charIndex);
 
         charIndex++;
 
-        setTimeout(typeEffect,100);
-    }
-    else{
+        setTimeout(typeEffect, 100);
 
-        setTimeout(()=>{
+    } else {
 
-            document.getElementById("typing").innerHTML="";
-            charIndex=0;
-            index=(index+1)%text.length;
+        setTimeout(() => {
+
+            document.getElementById("typing").innerHTML = "";
+            charIndex = 0;
+            index = (index + 1) % text.length;
 
             typeEffect();
 
-        },1500);
+        }, 1500);
     }
 }
 
 typeEffect();
-document.getElementById("moreProjectsBtn").addEventListener("click", function () {
 
-    const hiddenProjects = document.querySelectorAll(".more-project");
 
-    hiddenProjects.forEach(function(project) {
-        project.style.display = "block";
+// More Dashboards Button
+
+const moreProjectsBtn = document.getElementById("moreProjectsBtn");
+
+if (moreProjectsBtn) {
+
+    moreProjectsBtn.addEventListener("click", function () {
+
+        const hiddenProjects =
+            document.querySelectorAll(".more-project");
+
+        hiddenProjects.forEach(function (project) {
+            project.style.display = "block";
+        });
+
+        this.style.display = "none";
     });
-
-    this.style.display = "none";
-
-});
+}
